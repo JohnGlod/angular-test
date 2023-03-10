c0e93e3382f45b3e8ee0a576bbf1016c6b507f7d
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-03-10 22:36:31 +0500

    feat: create card item

--- app/src/app/components/card-item/card-item.component.ts
+++ app/src/app/components/card-item/card-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IMovie } from 'src/app/models/IMovie.intarface';
 
 @Component({
   selector: 'card-item',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class CardItemComponent {
+  @Input() item: IMovie
 }
