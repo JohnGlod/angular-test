78ccc3a305ce36d92ef5a4f8ace33091b64cffe8
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-03-10 22:38:19 +0500

    feat: update app files

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 import data from './data/data.json'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'app';
   movies = data
+  bestMovie = [];
+  
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardItemComponent } from './components/card-item/card-item.component';
+import { SvgComponent } from './components/svg-like/svg-like.component';
+import { GenresTransformPipe, JoinPipe} from './pipes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardItemComponent
+    GenresTransformPipe,
+    JoinPipe,
+    CardItemComponent,
+    SvgComponent
   ],
   imports: [
     BrowserModule,
