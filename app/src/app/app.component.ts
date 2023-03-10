050d0618487ea8e8a03b54a5fe29675ff8c1299c
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-03-10 23:58:24 +0500

    feat: add bestMovie

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component,  } from '@angular/core';
-import data from './data/data.json'
+import { Component } from '@angular/core';
+import data from './data/data.json';
+import { IMovie } from './models/IMovie.intarface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
   title = 'app';
-  movies = data
-  bestMovie = [];
-  
+  movies = data;
+  bestMovie: IMovie[] = JSON.parse(
+    localStorage.getItem('favoriteMovie') ?? '[]'
+  );
+
+  private clearLocalStorage() {
+    localStorage.removeItem('favoriteMovie');
+  }
+
+  private setLocalStorage = (data: IMovie) => {
+    localStorage.setItem('favoriteMovie', JSON.stringify([data]));
+  };
+
+  addFavorite = (id: number): void => {
+    const findId = this.bestMovie.pop()?.id;
+
+    if (findId === id) {
+      this.clearLocalStorage();
+      return;
+    }
+
+    if (this.bestMovie.length > 0) {
+      this.clearLocalStorage();
+    }
+
+    const findMovie = this.movies.find((movie) => movie.id === id);
+
+    if (findMovie) {
+      this.setLocalStorage(findMovie);
+    }
+  };
 }
