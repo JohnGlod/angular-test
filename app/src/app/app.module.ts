11a772b65a181f9a1c206f0e033727422c4f98b4
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-03-10 17:05:12 +0500

    init: clear project

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
