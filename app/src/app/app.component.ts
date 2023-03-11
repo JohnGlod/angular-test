bedb034eab67e843d99ccc2262afb08f564ecd19
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-03-11 15:57:49 +0500

    refactor: add storage service

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,42 +1,37 @@
 import { Component } from '@angular/core';
 import data from './data/data.json';
 import { IMovie } from './models/IMovie.intarface';
+import { StorageService } from './service/storage.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
+  constructor(private Storage: StorageService){}
   title = 'app';
   movies = data;
-  bestMovie: IMovie[] = JSON.parse(
-    localStorage.getItem('favoriteMovie') ?? '[]'
-  );
-
-  private clearLocalStorage() {
-    localStorage.removeItem('favoriteMovie');
-  }
-
-  private setLocalStorage = (data: IMovie) => {
-    localStorage.setItem('favoriteMovie', JSON.stringify([data]));
-  };
+  bestMovie: IMovie[] = this.Storage.getLocalStorage();
 
   addFavorite = (id: number): void => {
     const findId = this.bestMovie.pop()?.id;
 
     if (findId === id) {
-      this.clearLocalStorage();
+      this.Storage.clearLocalStorage();
       return;
     }
 
     if (this.bestMovie.length > 0) {
-      this.clearLocalStorage();
+      this.Storage.clearLocalStorage();
     }
 
     const findMovie = this.movies.find((movie) => movie.id === id);
 
     if (findMovie) {
-      this.setLocalStorage(findMovie);
+      this.Storage.setLocalStorage([findMovie]);
     }
   };
+
 }
