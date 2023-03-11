b5b555899a842ae59f4e6bbcb89a5de4fa6ec5c8
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-03-11 21:07:21 +0500

    feat: update card-item

--- app/src/app/components/card-item/card-item.component.ts
+++ app/src/app/components/card-item/card-item.component.ts
@@ -4,11 +4,11 @@ import { IMovie } from 'src/app/models/IMovie.intarface';
 @Component({
   selector: 'card-item',
   templateUrl: './card-item.component.html',
-  styleUrls: ['./card-item.component.scss']
+  styleUrls: ['./card-item.component.scss'],
 })
-
 export class CardItemComponent {
   @Input() item: IMovie;
-  @Input() liked: boolean;
+  @Input() isLiked: boolean;
+  @Input() isLikedId?: number;
   @Input() addFavorite: (id: number) => void;
 }
