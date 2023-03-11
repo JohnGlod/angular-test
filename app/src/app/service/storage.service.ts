6e745dace98027d9b061b97c978971982d49e9a2
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-03-11 21:12:47 +0500

    feat: add sync storage with rxjs

--- app/src/app/service/storage.service.ts
+++ app/src/app/service/storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { IMovie } from '../models/IMovie.intarface';
 
 export const MOVIE_FROM_LOCAL_STORAGE_KEY = 'favoriteMovie';
@@ -6,19 +7,33 @@ export const MOVIE_FROM_LOCAL_STORAGE_KEY = 'favoriteMovie';
 @Injectable({
   providedIn: 'root',
 })
-export class StorageService {
-  constructor() {}
+export class LocalStorageService {
+  private readonly synchronizedData$ = new BehaviorSubject<IMovie[]>([]);
 
-  clearLocalStorage() {
-    localStorage.removeItem(MOVIE_FROM_LOCAL_STORAGE_KEY);
-  }
-
-  setLocalStorage = (data: IMovie[]) => {
-    localStorage.setItem(MOVIE_FROM_LOCAL_STORAGE_KEY, JSON.stringify(data));
-  };
-  getLocalStorage(): IMovie[] {
-    return JSON.parse(
+  constructor() {
+    const data = JSON.parse(
       localStorage.getItem(MOVIE_FROM_LOCAL_STORAGE_KEY) ?? '[]'
     );
+    this.synchronizedData$.next(data);
+    window.addEventListener('storage', (event) => {
+      if (event.key === MOVIE_FROM_LOCAL_STORAGE_KEY) {
+        const data = JSON.parse(event.newValue as string);
+        this.synchronizedData$.next(data);
+      }
+    });
+  }
+
+  setSynchronizedData(data: IMovie[]) {
+    localStorage.setItem(MOVIE_FROM_LOCAL_STORAGE_KEY, JSON.stringify(data));
+    this.synchronizedData$.next(data);
+  }
+
+  clearLocalStorage() {
+    localStorage.setItem(MOVIE_FROM_LOCAL_STORAGE_KEY, JSON.stringify([]));
+    this.synchronizedData$.next([]);
+  }
+
+  getSynchronizedData() {
+    return this.synchronizedData$.asObservable();
   }
 }
